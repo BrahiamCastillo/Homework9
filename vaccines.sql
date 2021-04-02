251f453a1e5052cab83a3177e9dd77c5ad38fc06
felix281202 <felix_infanteramirez28@yahoo.com>
2021-04-02 15:08:21 -0400

    Parte de registrar falta arreglar los foreach y lo del covid y sangre

--- vaccines.sql
+++ vaccines.sql
@@ -1,30 +1,30 @@
 CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
-    `MigrationId` varchar(95) NOT NULL,
-    `ProductVersion` varchar(32) NOT NULL,
+    `MigrationId` VARCHAR(95) NOT NULL,
+    `ProductVersion` VARCHAR(32) NOT NULL,
     CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
 );
 
 CREATE TABLE `Sangre` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `tipo_sangre` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `tipo_sangre` VARCHAR(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
     CONSTRAINT `PK_Sangre` PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `Persona` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `Sangre_id` int(11) NOT NULL,
-    `cedula` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `nombre` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `apellido` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `telefono` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `correo` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `fecha_nacimiento` date NOT NULL,
-    `provincia` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `direccion` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `latitud` double NOT NULL,
-    `longitud` double NOT NULL,
-    `covid` tinyint(4) NOT NULL,
-    `justificacion` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `Sangre_id` INT(11) NOT NULL,
+    `cedula` VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `nombre` VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `apellido` VARCHAR(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `telefono` VARCHAR(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `correo` VARCHAR(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `fecha_nacimiento` DATE NOT NULL,
+    `provincia` VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `direccion` VARCHAR(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `latitud` DOUBLE NOT NULL,
+    `longitud` DOUBLE NOT NULL,
+    `covid` TINYINT(4) NOT NULL,
+    `justificacion` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
     CONSTRAINT `PRIMARY` PRIMARY KEY (`id`, `Sangre_id`),
     CONSTRAINT `fk_Persona_Sangre` FOREIGN KEY (`Sangre_id`) REFERENCES `Sangre` (`id`) ON DELETE RESTRICT
 );
@@ -52,4 +52,6 @@ INSERT INTO `Sangre` (`id`, `tipo_sangre`)
 VALUES (8, 'AB-');
 
 INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
-VALUES ('20210401164600_seed_bloods', '3.1.12');
+VALUES ('20210401164600_`__efmigrationshistory``persona`seed_bloods', '3.1.12');
+USE vaccines
+SELECT * FROM persona
