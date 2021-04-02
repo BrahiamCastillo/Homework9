719770bfddf3377548c6628953dadb201c99402a
felix281202 <felix_infanteramirez28@yahoo.com>
2021-04-02 15:11:51 -0400

    parte de registrar falta arreglar lo del covid y sangre ya que los puse como un select

--- obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Registrar.razor.g.cs
+++ obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Registrar.razor.g.cs
@@ -11,84 +11,84 @@ namespace Homework9.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\felix\Documents\Homework9\_Imports.razor"
+#line 1 "c:\Users\felix\Documents\Homework9\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\felix\Documents\Homework9\_Imports.razor"
+#line 2 "c:\Users\felix\Documents\Homework9\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\felix\Documents\Homework9\_Imports.razor"
+#line 3 "c:\Users\felix\Documents\Homework9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\felix\Documents\Homework9\_Imports.razor"
+#line 4 "c:\Users\felix\Documents\Homework9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\felix\Documents\Homework9\_Imports.razor"
+#line 5 "c:\Users\felix\Documents\Homework9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\felix\Documents\Homework9\_Imports.razor"
+#line 6 "c:\Users\felix\Documents\Homework9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\felix\Documents\Homework9\_Imports.razor"
+#line 7 "c:\Users\felix\Documents\Homework9\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\felix\Documents\Homework9\_Imports.razor"
+#line 8 "c:\Users\felix\Documents\Homework9\_Imports.razor"
 using Homework9;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\felix\Documents\Homework9\_Imports.razor"
+#line 9 "c:\Users\felix\Documents\Homework9\_Imports.razor"
 using Homework9.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\felix\Documents\Homework9\Pages\Registrar.razor"
+#line 2 "c:\Users\felix\Documents\Homework9\Pages\Registrar.razor"
 using System;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\felix\Documents\Homework9\Pages\Registrar.razor"
+#line 3 "c:\Users\felix\Documents\Homework9\Pages\Registrar.razor"
 using System.Linq;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\felix\Documents\Homework9\Pages\Registrar.razor"
+#line 4 "c:\Users\felix\Documents\Homework9\Pages\Registrar.razor"
 using Homework9.Models;
 
 #line default
@@ -103,7 +103,7 @@ using Homework9.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 87 "C:\Users\felix\Documents\Homework9\Pages\Registrar.razor"
+#line 87 "c:\Users\felix\Documents\Homework9\Pages\Registrar.razor"
  
     string cedula;
     string nombre;
