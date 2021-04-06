edef5ef1860c7bd477d0c08fcfb696595ae5057c
BrahiamCastillo <mymoviehb@gmail.com>
2021-04-06 13:26:10 -0400

    Register finish

--- wwwroot/scripts/mapadd.js
+++ wwwroot/scripts/mapadd.js
@@ -1,8 +1,29 @@
 let map;
 
 function initMap() {
+    const myLatlng = { lat: 18.735693, lng: -70.162651 };
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
+        center: myLatlng,
         zoom: 8,
     });
+
+    let infoWindow = new google.maps.InfoWindow({
+        content: "Click al mapa para obtener latitud y longitud",
+        position: myLatlng,
+    });
+    infoWindow.open(map);
+    // Configure the click listener.
+    map.addListener("click", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+        infoWindow.close();
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+        });
+        infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(map);
+    });
+
 }
